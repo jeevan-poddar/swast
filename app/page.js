fcaba688e745e5bc79e0bf14bfba435e9943cedf
jeevan-poddar <g1prodev@gmail.com>
2026-03-23 21:07:49 +0530

    check

--- app/page.js
+++ app/page.js
@@ -1,7 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return( 
-  <div className="">I am home</div>
-);
+  return (
+    <div className="">
+      I am home
+      <div className="">
+        Click to go to <Link href="/signin">signin</Link>
+      </div>
+      <div className="">
+        Click to go to <Link href="/signup">signup</Link>
+      </div>
+    </div>
+  );
 }
